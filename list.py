7cdef9194539d14bea89f4c9cf4bd8dadf0d4ac1
Shreyas200408 <shreyasshreyas0132@gmail.com>
2025-10-09 12:09:55 +0530

    Add files via upload
    Data structure and example

---
+++ list.py
@@ -0,0 +1,32 @@
+list=[45,12,54,10,5,12,12,12]
+# print(list)
+
+# list.append(25)
+# print(list)
+
+list.count(12)
+print(list)
+
+# list.index(10)
+# print(list)
+
+# list.insert()
+# print(list)
+
+# list.pop()
+# print(list)
+
+# list.reverse()
+# print(list)
+
+# list.remove(45)
+# print(list)
+
+# list.sort()
+# print(list)
+
+# list.copy()
+# print(list)
+
+# list.clear()
+# print(list)

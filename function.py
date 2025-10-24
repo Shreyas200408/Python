ac146584928c6aafa3465899b116af1a82e93117
Shreyas200408 <shreyasshreyas0132@gmail.com>
2025-10-24 19:21:41 +0530

    Add files via upload
    Functions and Loops

---
+++ function.py
@@ -0,0 +1,69 @@
+# def age():
+#     return age
+# age = int(input("enter your age : "))
+
+
+# def addition(a,b):
+#     return a + b
+
+# a= int(input("enter a value : "))
+
+# b = int(input("enter b value : "))
+
+# result = addition(a,b)
+
+# print(result)
+
+
+# def calcAge(birthYear):
+#     return 2025 - birthYear
+
+
+# userAgeInput = int(input("Enter Your BirthYear: "))
+
+# userAge = calcAge(userAgeInput)
+# print(f"Your Age is {userAge}")
+
+
+
+
+# Function: Named set of Instructions to perfotm the Specific tasks
+# re-usable blocks
+
+# syntax : def fnName(): 
+                # code to be executed
+            # fnName()
+
+# def greet():
+#     print("Hello Good Morning")
+
+# greet() #function calling using Function name
+# greet()
+# greet()
+# greet()
+# greet()
+# greet()
+
+# Fn Parameters : they are the storage classes, which acts as a Variables to store the Data.
+# fn Arumnets : Values which passed onto the Parameters
+# def greet(name,msg):
+#     print(f"Hello {name}, {msg}")
+
+# # greet("Manoj")
+# # greet("Vivek")
+# # greet()
+
+# greet("Manoj","Uta Aytha..?")
+# greet("Shreyas","Nidde Madbeda")
+
+# def addition(a,b):
+#     return a + b  # return : used to get the values from the Fucntion (it returns the Value)
+
+# # result = addition(10,20)
+
+# aInput = int(input("Enter the Value of a :  ")) 
+# bInput = int(input("Enter the Value of b :  ")) 
+
+
+# result = addition(aInput,bInput)
+# print(result)

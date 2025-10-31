37bc5007bad5a0f4481c939a0650c3628c0ce333
Shreyas200408 <shreyasshreyas0132@gmail.com>
2025-10-31 20:51:09 +0530

    conditions

---
+++ condition.py
@@ -0,0 +1,100 @@
+# Condtions :
+# if : if the condtion is True
+# if else : if the condition is False then the else block will execute
+# elif (Nested If) : to Handle the Multiple Conditions
+
+# if:
+# Syntax :
+
+# age = 19
+
+# if(age>=18):
+#     print("You can Vote")
+
+# else:
+#     print("You cannot Vote")
+
+
+# elif : to Handle the Multiple COnditions
+
+# marks = int(input("Enter Your Marks :  "))
+
+# if(marks>=85):
+#     print("A")
+
+# elif(marks>=65):
+#     print("B")
+
+# elif(marks>=35):
+#     print("C")
+
+# else:
+#     print("F")
+
+
+# Logical Operators with COndtions
+# and , or, not
+
+# Driving Criteria using conditions and Logics
+
+haveDL = True
+haveGoodVision = False
+
+if(haveDL or haveGoodVision):
+    print("You can Drive")
+
+else:
+    print("You cannot Drive")
+
+
+# SpeedLimit Checking on Fine
+
+# vehicleSpeed = 260
+
+# if(vehicleSpeed <= 100):
+#     print("No Fine")
+
+# elif(vehicleSpeed > 100 and vehicleSpeed < 150):
+#     print("1000 rupees fine😥")
+
+# elif(vehicleSpeed >150 and vehicleSpeed < 200):
+#     print("2000 rs Fine")
+
+# else:
+#     print("Seize the Vehicle")
+
+
+# Login Functionality 
+# userName
+# Password
+
+# userName = 'Manoj@20'
+# password = 'ManojAcube'
+
+# userNameInput = input("Enter userName :  ")
+# userPasswordInput = input("Enter the Password :  ")
+
+
+# if(userNameInput == 'Manoj@20'):
+#     print("user name correct")
+
+# else:
+#     print("Invalid user name")
+
+# if(userPasswordInput == 'ManojAcube'):
+#     print("password correct")
+
+# else:
+#     print("Invalid password")
+
+# if(userNameInput == 'Manoj@20' and userPasswordInput == 'ManojAcube'):
+#     print("Login Successfull..!")
+
+# else:
+#     print("Invalid credentials")
+
+# if(userNameInput ==userName and userPasswordInput == password):
+#     print("Login Successfull")
+
+# elif(userNameInput == userName and userPasswordInput != password):
+#     print("Password Invalid")
